94de1f404e77f2545f0e1d4560862137fe0ff661
YoYag <la2043432@gmail.com>
2022-07-05 11:34:20 +0900

    DB 게시물 정보 출력

---
+++ src/main/java/com/mysite/sbb/Article/controller/ArticleController.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb.Article.controller;
+
+import com.mysite.sbb.Article.dao.ArticleRepository;
+import com.mysite.sbb.Article.domain.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/user/article")
+public class ArticleController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<Article> articles() {
+        return articleRepository.findAll();
+    }
+}
