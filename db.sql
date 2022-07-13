cefdadf4578dfd7e4a81777181ccb9aae1a86126
YoYag <la2043432@gmail.com>
2022-07-13 10:46:52 +0900

    질문 db쿼리 추가 및 조회

--- db.sql
+++ db.sql
@@ -68,5 +68,30 @@ title = '제목 3',
 `body` = '내용 3',
 `user_id` = 3;
 
+# 질문 테이블 생성
+CREATE TABLE `question` (
+    id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    create_date DATETIME NOT NULL,
+    `subject` VARCHAR(200) NOT NULL,
+    `content` TEXT NOT NULL
+);
+
+# 질문데이터 생성
+INSERT INTO `question`
+SET create_date = NOW(),
+`subject` = '질문 1',
+`content` = '질문내용 1';
+
+INSERT INTO `question`
+SET create_date = NOW(),
+`subject` = '질문 2',
+`content` = '질문내용 2';
+
+INSERT INTO `question`
+SET create_date = NOW(),
+`subject` = '질문 3',
+`content` = '질문내용 3';
+
 SELECT * FROM `user`;
 SELECT * FROM `article`;
+SELECT * FROM `question`;
