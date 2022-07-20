4725258ed3ecfc78516f122c7843287d4f8078cd
YoYag <la2043432@gmail.com>
2022-07-20 13:17:50 +0900

    질문 등록 기능 추가 및 Spring_Boot_Validation 설치하여 폼 클래스 활용

--- src/main/java/com/mysite/sbb/answer/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/controller/AnswerController.java
@@ -1,15 +1,18 @@
 package com.mysite.sbb.answer.controller;
 
+import com.mysite.sbb.answer.form.AnswerForm;
 import com.mysite.sbb.answer.service.AnswerService;
 import com.mysite.sbb.question.domain.Question;
 import com.mysite.sbb.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -20,9 +23,13 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Long id, @RequestParam String content) {
+    public String createAnswer(Model model, @PathVariable("id") Long id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 }
--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -1,13 +1,19 @@
 package com.mysite.sbb.question.controller;
 
+import com.mysite.sbb.answer.form.AnswerForm;
 import com.mysite.sbb.question.domain.Question;
+import com.mysite.sbb.question.form.QuestionForm;
 import com.mysite.sbb.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -25,9 +31,23 @@ public class QuestionController {
     }
 
     @RequestMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Long id) {
+    public String detail(Model model, @PathVariable("id") Long id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
